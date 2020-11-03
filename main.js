57e46d7b1ff5d2705d08123f20eef9abd79752db
Asaf Läckgren <asaf_lackgren@hotmail.com>
2020-11-03 10:46:22 +0100

    add jsdoc comments

--- main.js
+++ main.js
@@ -6,26 +6,26 @@ function main(){
     startSlideShowAnimation();
 }
 
+/** starts the animation with set interval */
 function startSlideShowAnimation(){
     setInterval(changeActiveImage, 2000);
     
 }
 
+/** iterates through images and applies class for opacity */
 function changeActiveImage(){
     const images = document.querySelectorAll('.container > img');
     console.log(images);
     for(let i = 0;i < images.length; i++){
         if (images[i].classList.contains('show')) {
             images[i].classList.remove('show');
-            //1. take away show 
 
+            //makes sure that we go back to zero when index passes images length. 
             let nextIndex = i + 1;
             if (nextIndex >= images.length){
                 nextIndex = 0;
             }
             images[nextIndex].classList.add('show');
-            //2. add show to next index
-
             break;
         }
     }
